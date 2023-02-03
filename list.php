98d0cb11912251b0c25c54ba9184fa6a430b850c
rudenzstore <121354139+rudenzstore@users.noreply.github.com>
2023-02-03 21:56:33 +0700

    Add files via upload

---
+++ list.php
@@ -0,0 +1,31 @@
+<?php
+
+// REDIRECT TO WA
+  $nomor = $data->nowa;
+
+
+
+  $send = "https://wa.me/62895386116581?text=HALO BOT";
+
+
+
+  }
+  
+
+
+
+
+
+
+
+
+} else {
+  $error = "";
+  $validid = "";
+}
+
+
+ ?>
+
+
+
